e28d32a93a353eb007dab6688f135a7e59118ed3
Emmanuel Apau <kave@users.noreply.github.com>
2018-10-12 14:48:30 -0400

    guestbook/istio

---
+++ istio_config.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# Local Istio
+export INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="http2")].nodePort}')
+export SECURE_INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="https")].nodePort}')
+export INGRESS_HOST=$(kubectl get po -l istio=ingressgateway -n istio-system -o 'jsonpath={.items[0].status.hostIP}')
+export GATEWAY_URL=$INGRESS_HOST:$INGRESS_PORT
